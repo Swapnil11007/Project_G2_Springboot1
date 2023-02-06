a4fcf19a239bb7d240477386898044a19a57d003
tej140 <tejasai9124@gmail.com>
2023-02-06 17:19:20 +0530

    Created User Model and Added DAO and controller for it.

---
+++ src/main/java/com/model/Users.java
@@ -0,0 +1,75 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Users {
+	@Id@GeneratedValue
+	private int userId;
+	private String userName;
+	@Column(unique=true)
+	private String emailId;
+	private String mobileNumber;
+	private String password;
+	
+	public Users() {
+		super();
+	}
+	
+	public Users(int userId, String userName, String emailId, String mobileNumber, String password) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.emailId = emailId;
+		this.mobileNumber = mobileNumber;
+		this.password = password;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", userName=" + userName + ", emailId=" + emailId + ", mobileNumber="
+				+ mobileNumber + ", password=" + password + "]";
+	}
+	
+	
+
+}
